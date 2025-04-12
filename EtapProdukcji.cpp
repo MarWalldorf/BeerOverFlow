a9cb3a06a47c20aefe47a2a5ce30b415bdd5a7f8
Martin1337 <115101060+MarWalldorf@users.noreply.github.com>
2025-04-12 19:26:33 +0200

    Update EtapProdukcji.cpp

--- EtapProdukcji.cpp
+++ EtapProdukcji.cpp
@@ -2,11 +2,10 @@
 #include <sstream>
 using namespace std;
 
-EtapProdukcji::EtapProdukcji(string opis, int czasMinut)
-    : opis(move(opis)), czasMinut(czasMinut) {
-}
+EtapProdukcji::EtapProdukcji(string opis, int czasMinut): opis(move(opis)), czasMinut(czasMinut) {}
 
-string EtapProdukcji::pobierzOpis() const {
+string EtapProdukcji::pobierzOpis() const 
+{
     ostringstream oss;
     oss << opis << " (" << czasMinut << " minut)";
     return oss.str();
